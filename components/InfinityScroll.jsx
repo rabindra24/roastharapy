6aeb191291cecec781a919f2a0ec71c95d598b16
rabin2403 <rabindra2402@gmail.com>
2023-10-16 10:55:29 +0530

    aco0

--- components/InfinityScroll.jsx
+++ components/InfinityScroll.jsx
@@ -18,11 +18,11 @@ const InfinityScroll = () => {
         ul.insertAdjacentHTML('afterend', ul.outerHTML);
         ul.nextSibling.setAttribute('aria-hidden', 'true');
     })"
-      class="w-full inline-flex flex-nowrap overflow-hidden "
+      className="w-full inline-flex flex-nowrap overflow-hidden "
     >
       <ul
         x-ref="logos"
-        class="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
+        className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
       >
         {data.map((item) => (
           <li key={item.id}>
@@ -32,7 +32,7 @@ const InfinityScroll = () => {
       </ul>
       <ul
         x-ref="logos"
-        class="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
+        className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
       >
         {data.map((item) => (
           <li key={item.id}>
